597675db924c94550d3b38463140a5827660c1e7
Niklas Thomsen <nooblas@hotmail.com>
2020-08-22 10:51:17 +0200

    Added wordcounter method

--- wordCounter.py
+++ wordCounter.py
@@ -1,2 +1,12 @@
 class wordCounter():
-    def wordCount(input):
+    def wordCount(self, inputWords = ""):
+        counts = dict()
+        words = inputWords.split()
+        
+        for word in words:
+            if word in counts:
+                counts[word] += 1
+            else:
+                counts[word] = 1
+
+        return counts
