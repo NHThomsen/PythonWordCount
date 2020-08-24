daaab98768f9c8bcab45fbfd0955ae64ea5a8600
Niklas Thomsen <nooblas@hotmail.com>
2020-08-24 12:04:52 +0200

    Added post processing removal

--- wordCounter.py
+++ wordCounter.py
@@ -14,5 +14,22 @@ class wordCounter():
             return counts
         else:
             return ""
+    def wordCountPostProcessing(self, textToCount = ""):
+        if textToCount != "":
+            counts = dict()
+            words = textToCount.split()
+
+            for word in words:
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word]  = 1
+            copiedCounts = dict(counts)
+            for word in copiedCounts:
+                if word in self.wordsToRemoveSet:
+                    del counts[word]
+            return counts
+        else:
+            return ""
     def wordsToRemove(self, removeWords):
         self.wordsToRemoveSet = removeWords
